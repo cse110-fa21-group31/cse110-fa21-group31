779f0519296a66b3d493c8cda671326deb15a83c
celiaxiao <c6xiao@ucsd.edu>
2021-11-15 11:59:14 -0800

    define service API

---
+++ source/service/neDB/interface.js
@@ -0,0 +1,23 @@
+function createRecipe(recipe) {
+
+}
+
+function deleteRecipe(id) {
+
+}
+
+function updateRecipe(id, recipe) {
+
+}
+
+function getAllRecipe() {
+
+}
+
+function getRecipeById() {
+
+}
+
+function getRecipeByIds(ids) {
+
+}
