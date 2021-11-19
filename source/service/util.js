43534dcc0718eb30ee8e20cbf9f639e360775982
celiaxiao <c6xiao@ucsd.edu>
2021-11-19 12:52:02 -0800

    define backend API, implement getOne, create

--- source/service/util.js
+++ source/service/util.js
@@ -1,2 +1,6 @@
 // the file that used to put reusable helper methods and/or constants
 //eg: PORT, neDB storage File Path, etc
+
+export const RECIPE_DB_PATH = "../data/recipes"
+
+// export { RECIPE_DB_PATH }
