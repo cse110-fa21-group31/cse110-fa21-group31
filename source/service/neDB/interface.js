c1cd0a61de2a16330e85d963276913d759b4faa6
Kian <kezaz@ucsd.edu>
2021-11-21 14:04:44 -0800

    work on routes

--- source/service/neDB/interface.js
+++ source/service/neDB/interface.js
@@ -1,23 +1,36 @@
 /* eslint-disable no-unused-vars*/
-function createRecipe(recipe) {}
+function createRecipe(recipe, recipeCollection) {
+    const insertedId = await recipeCollection.insertOne(recipe).insertedId;
+    recipe._id = insertedId;
+    return recipe;
+}
 /* eslint-enable no-unused-vars*/
 
 /* eslint-disable no-unused-vars*/
-function deleteRecipe(id) {}
+function deleteRecipe(id, recipeCollection) {}
 /* eslint-enable no-unused-vars*/
 
 /* eslint-disable no-unused-vars*/
-function updateRecipe(id, recipe) {}
+function updateRecipe(id, recipe, recipeCollection) {}
 /* eslint-enable no-unused-vars*/
 
 /* eslint-disable no-unused-vars*/
-function getAllRecipe() {}
+function getAllRecipe(recipeCollection) {
+    const allRecipes;
+    const recipesCursor = recipeCollection.find();
+    recipesCursor.foreach(recipe => {
+        allRecipes.push(recipe);
+    });
+    return allRecipes;
+}
 /* eslint-enable no-unused-vars*/
 
 /* eslint-disable no-unused-vars*/
-function getRecipeById() {}
+function getRecipeById(id, recipeCollection) {
+
+}
 /* eslint-enable no-unused-vars*/
 
 /* eslint-disable no-unused-vars*/
-function getRecipeByIds(ids) {}
+function getRecipeByIds(ids, recipeCollection) {}
 /* eslint-enable no-unused-vars*/
--- source/service/neDB/server.js
+++ source/service/neDB/server.js
@@ -1,6 +1,10 @@
+const Datastore = require("nedb");
+const recipeCollection = new Datastore({ filename: "source/service/data/recipes", autoload: true});
+const userCollection = new Datastore({ filename: "source/service/data/users", autoload: true}); 
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
 const port = process.env.PORT || 3030;
+
 // Declare a route
 fastify.get("/", async () => {
     // recipe = request.body
@@ -8,10 +12,47 @@ fastify.get("/", async () => {
     return { hello: "world" };
 });
 
+/*
 fastify.get("/api", async (_, reply) => {
     const fs = require("fs");
     const stream = fs.createReadStream("./index.html");
     reply.type("text/html").send(stream);
+});
+*/
+
+fastify.get("/api", async (request, reply) => {
+    if (request.query.id) {
+        reply.send(getRecipeById(request.query.id, recipeCollection));
+    }
+    else if (request.query.page) {
+        reply.send(getAllRecipe());
+    }
+});
+
+fastify.post("/api", async (request, reply) => {
+    if (!request.body.name || !request.body.author ||!request.body.steps) {
+        const err = new Error();
+        err.statusCode = 400;
+        reply.send(err);
+    }
+    else {
+        reply.send(createRecipe(request.body, recipeCollection));
+    }
+});
+
+fastify.put("/api", async (request, reply) => {
+    if (!request.body.name || !request.body.author ||!request.body.steps) {
+        const err = new Error();
+        err.statusCode = 400;
+        reply.send(err);
+    }
+    else {
+        reply.send(updateRecipe(req.body.id, req.body, recipeCollection));
+    }
+});
+
+fastify.delete("/api", async (request, reply) => {
+    reply.send(deleteRecipe(req.query.id, recipeCollection));
 });
 
 // Run the server!
