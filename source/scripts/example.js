40689b6490e00428b4e3db37364d3e4dea7599cc
euvinkeel <emkeel@ucsd.edu>
2021-11-20 17:43:48 -0800

    Add JSDoc annotation to example.js, try to implement caching in workflow

--- source/scripts/example.js
+++ source/scripts/example.js
@@ -1,6 +1,13 @@
 // example.js
 module.exports = getHelloWorld;
 
+/**
+ * Returns a string that says "Hello World!".
+ * @example
+ * //returns "Hello World!"
+ * getHelloWorld();
+ * @returns {string}
+ */
 function getHelloWorld() {
   return "Hello World!";
 }
