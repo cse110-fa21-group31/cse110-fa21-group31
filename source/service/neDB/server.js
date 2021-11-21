b29825c3b9a42e68c77789477ef68c24ebfd8ecc
celiaxiao <c6xiao@ucsd.edu>
2021-11-20 23:59:59 -0800

    server support serving static files; router research needed

--- source/service/neDB/server.js
+++ source/service/neDB/server.js
@@ -1,9 +1,17 @@
+
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 const port = process.env.PORT || 3030;
 // Declare a route
 fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
+})
+
+
+fastify.get('/api', async (request, reply) => {
+    const fs = require('fs')
+    const stream = fs.createReadStream('./index.html')
+    reply.type('text/html').send(stream)
 })
 
 // Run the server!
