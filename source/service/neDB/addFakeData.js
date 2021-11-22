30f25b62522ee2248d91197b65fc03142119d985
Xin Sheng <xisheng@ucsd.edu>
2021-11-21 18:36:43 -0800

    Updated addFakeData.js for testing

--- source/service/neDB/addFakeData.js
+++ source/service/neDB/addFakeData.js
@@ -1,17 +1,10 @@
 var Datastore = require("nedb");
 // var db = new Datastore({ filename: "data/demo" });
 // const demo2 = new Datastore({ filename: "data/demo2", autoload: true });
-<<<<<<< HEAD
-const RECIPE_DB_PATH = "source/service/.data/recipes";
-// const USER_DB_PATH = "source/service/.data/users"
-const recipeDB = new Datastore({ filename: RECIPE_DB_PATH, autoload: true });
-// const userDB = new Datastore({ filename: USER_DB_PATH, autoload: true });
-=======
 // const RECIPE_DB_PATH = "source/service/.data/recipes"
 const USER_DB_PATH = "source/service/.data/users"
 // const recipeDB = new Datastore({ filename: RECIPE_DB_PATH, autoload: true });
 const userDB = new Datastore({ filename: USER_DB_PATH, autoload: true });
->>>>>>> d48a8e0 (Finished userInterface)
 // db.loadDatabase(function (err) {
 //     // Start issuing commands after callback...
 //     if (!err) {
