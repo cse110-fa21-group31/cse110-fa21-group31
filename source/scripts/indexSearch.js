60cedd6326473b0514d928523f60ee4f4eef99fb
helloserina <skhanna@ucsd.edu>
2021-11-21 19:25:48 -0800

    resolve conflicts

--- source/scripts/indexSearch.js
+++ source/scripts/indexSearch.js
@@ -1,25 +1,30 @@
 //This file is when the user searches a keyword in index.html page
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 const typeInput = {};
 const tagsInput = {};
 
 async function init() {
+    console.log(window.location);
 
-    //console.log(window.location);
-
-    if (document.querySelector('#keywordSearch')) {
-        document.querySelector('#keywordSearch').querySelector('input').addEventListener('change', updateKeywords);
+    if (document.querySelector("#keywordSearch")) {
+        document
+            .querySelector("#keywordSearch")
+            .querySelector("input")
+            .addEventListener("change", updateKeywords);
     }
 
-    if ( document.querySelector('#submit')) {
-        document.querySelector('#submit').querySelector('input').addEventListener('click', submitSearch);
+    if (document.querySelector("#submit")) {
+        document
+            .querySelector("#submit")
+            .querySelector("input")
+            .addEventListener("click", submitSearch);
     }
 }
 
 function updateKeywords(e) {
     e = String(e.target.value);
-    e = e.split(' ');
+    e = e.split(" ");
     // console.log(type(e));
     for (let i = 0; i < e.length; i++) {
         typeInput[i] = e[i];
@@ -29,6 +34,6 @@ function updateKeywords(e) {
 async function submitSearch(e) {
     // assign a "score" to how matching the input + tags is to a recipe's title + tags
     // window.location.href='/source/pages/homePage.html';
-    //console.log(e);
+    console.log(e);
     // e.preventDefault();
 }
