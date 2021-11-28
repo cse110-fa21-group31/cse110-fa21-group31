66ed27c444ad817b01e886f68a1c7ab8a21cf8a6
helloserina <skhanna@ucsd.edu>
2021-11-28 12:51:23 -0800

    pushing for tallis

--- source/scripts/indexSearch.js
+++ source/scripts/indexSearch.js
@@ -5,14 +5,64 @@ window.addEventListener("DOMContentLoaded", init);
 }
 import { submitSearch } from "./APICalls.js"
 
+let allTags = ["Easy", "Intermediate", "Hard", "Vegetarian", "Breakfast", "Dinner", "Appetizer", "Lunch", "Beverage"];
+let selectedTags = [];
+
 /**
  * initializes search functionality through the frontend
  */
 async function init() {
     let searchBar = document.querySelector("#searchBar");
     let searchButton = document.querySelector("#searchButton");
-    searchButton.addEventListener("click", function () {
-        // TODO: Add a tags field to the search bar and make use of it
-        submitSearch(searchBar.value);
-    });
+    if (searchButton) {
+        searchButton.addEventListener("click", function () {
+            // TODO: Add a tags field to the search bar and make use of it
+            submitSearch(searchBar.value);
+        });
+    }
+
+    let tagsSelect = document.getElementById("selectTagButton");
+    //console.log(tagsSelect);
+    if (tagsSelect) {
+        //console.log("found tag");
+        tagsSelect.addEventListener("click", async function () {
+            let response = await chooseTag();
+
+            let tagButtons = document.getElementsByClassName("tagButton");
+            tagButtons.forEach(tag => {
+                tag.addEventListener('click', clickedOnATag(tag));
+            });
+        });
+    }
+}
+
+async function chooseTag(e) {
+    if (document.getElementsByClassName("tagButton").length > 0) {
+        console.log(e);
+        clickedOnATag(e);
+    }
+    let tagsList = document.querySelector("#tagsList");
+    tagsList.style.display = "grid";
+    for (let i = 0 ; i < allTags.length; i++) {
+        let addTagButton = document.createElement("button");
+        addTagButton.classList.add("tagButton");
+        addTagButton.innerText = allTags[i];
+        addTagButton.id = allTags[i];
+        tagsList.appendChild(addTagButton);
+        //addTagButton.addEventListener("click", clickedOnATag(addTagButton));
+    }
+    // //document.getElementsByClassName("tagButton").addEventListener("click", clickedOnATag);
+    // const tagOptions = document.querySelectorAll('.tagButton');
+    // // adding the event listener by looping
+    // tagOptions.forEach(tag => {
+    //     tag.addEventListener('click', clickedOnATag(tag));
+    // });
+}
+
+function clickedOnATag(e) {
+    console.log(e);
+    console.log(e.id);
+    console.log(e.innerText);
+    selectedTags.push(e.innerText);
+    console.log(selectedTags);
 }
