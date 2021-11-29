8c76406a40530f37fd0faf5e1c1d9b9e3ec64bf6
Celia Xiao <56006273+celiaxiao@users.noreply.github.com>
2021-11-28 17:16:19 -0800

    Revert "Nav Bar with Google (#108)"
    This reverts commit 5bebf076f09f6cf1112f8ab2f09dea6d07c02f6b.

--- source/scripts/userInfo.js
+++ source/scripts/userInfo.js
@@ -1,21 +1,12 @@
 /**
  * This is the skeleton page to populate the user info page based on user profile json object
  */
-window.addEventListener("DOMContentLoaded", init);
 
-import getProfile from "/source/scripts/userLogin.js";
 import { routerAddCreatePage, routerNavigateWrapper } from "./index.js";
 import { EDIT_CREATE_ROUTE, USER_ROUTE } from "./util.js"
 // import { fetchUserById } from "./APICalls.js"
 export default {}
 const recipeData = {};
-
-async function init() {
-    let userInfoGrid = document.getElementsByClassName("userInfoGridItem");
-    console.log("Porofile");
-    //window[getProfile]();
-    console.log(getProfile());
-}
 
 /**
  * 
