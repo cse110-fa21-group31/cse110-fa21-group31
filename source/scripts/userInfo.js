01f1081475725f1335223c797ec3773ad2c576d5
TallisDawson <tdawson@ucsd.edu>
2021-12-09 23:57:23 -0800

    added documentation to userInfo

--- source/scripts/userInfo.js
+++ source/scripts/userInfo.js
@@ -1,27 +1,36 @@
 /**
- * This is the skeleton page to populate the user info page based on user profile json object
+ * Skeleton page to populate the user info page
+ * based on user profile json object.
+ * 
+ * @since 12.09.21
  */
 
-import { routerAddCreatePage, routerNavigateWrapper } from "./index.js";
-import { EDIT_CREATE_ROUTE, USER_ROUTE, createNodeClone } from "./util.js";
-// import { fetchUserById } from "./APICalls.js"
+import {
+    routerAddCreatePage,
+    routerNavigateWrapper,
+} from "./index.js";
+import { 
+    EDIT_CREATE_ROUTE,
+    createNodeClone,
+} from "./util.js";
+
 export default {};
-const recipeData = {};
 
 /**
- *
- * @param {*} profile user profile object defined in wiki
+ * Link routing to the create recipe button
+ * 
+ * @param {*} profile User profile object defined in wiki
  */
 export const populateUserInfoPage = (profile) => {
     const createPageUrl = EDIT_CREATE_ROUTE;
+
     //redirect to the create page
     createNodeClone("createRecipePageButton");
-    const createRecipePageBut = document.getElementById(
+    const createRecipePageButton = document.getElementById(
         "createRecipePageButton"
     );
-    //routerAddCreatePage(createPageUrl)
 
-    createRecipePageBut.addEventListener("click", () => {
+    createRecipePageButton.addEventListener("click", () => {
         routerAddCreatePage(createPageUrl);
         routerNavigateWrapper(createPageUrl);
     });
