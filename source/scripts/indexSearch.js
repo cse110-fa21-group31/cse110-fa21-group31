00c271cd45fb76ba821f8dff73d55d5c1a18d7a2
TallisDawson <tdawson@ucsd.edu>
2021-12-09 23:49:19 -0800

    Documented indexSearch.js

--- source/scripts/indexSearch.js
+++ source/scripts/indexSearch.js
@@ -1,30 +1,39 @@
-//This file is when the user searches a keyword in index.html page
+/**
+ * Populates tags and binds search functionality 
+ * to the search bar and tags.
+ * 
+ * @since 12.09.21
+ */
 import { createNodeClone } from "./util.js";
 export default { init, initialRecipeCards };
 if (typeof window === "object") {
     window.addEventListener("DOMContentLoaded", init);
 }
-import { submitSearch, submitInitialSearch } from "./APICalls.js";
+import {
+    submitSearch,
+    submitInitialSearch,
+ } from "./APICalls.js";
+
 import { createRecipeCards } from "./index.js";
 
-let allTags = ["Easy", "Intermediate", "Hard", "Vegetarian", "Breakfast", "Lunch", "Dinner", "Appetizer", "Dessert", "Vegan"];
-let difficultyTags = ["Easy", "Intermediate", "Hard"];
-let mealTimeTags = ["Breakfast", "Lunch", "Dinner", "Appetizer", "Dessert"];
-let dietaryTags = ["Vegetarian", "Vegan"];
+const difficultyTags = ["Easy", "Intermediate", "Hard"];
+const mealTimeTags = ["Breakfast", "Lunch", "Dinner", "Appetizer", "Dessert"];
+const dietaryTags = ["Vegetarian", "Vegan"];
 let selectedTags = [];
 let curr_page = 1;
 let max_page = 1;
+
 /**
- * initializes search functionality through the frontend
+ * Ties search functionality to the search bar, buttons and tags,
+ * populates tags and adds click event listener for styling and search.
  */
 async function init() {
-    // TODO: Make sure this works for both landing and home pages
+    
+    //Search bar and button
     createNodeClone("#searchBar", true);
-    let searchBar = document.querySelector("#searchBar");
     createNodeClone("#searchButton", true);
+    let searchBar = document.querySelector("#searchBar");
     let searchButton = document.querySelector("#searchButton");
-    let scrollLeftButton = document.querySelector("#scrollLeft");
-    let scrollRightButton = document.querySelector("#scrollRight");
 
     searchButton.addEventListener("click", async function () {
         submitNewSearch();
@@ -37,6 +46,10 @@ async function init() {
             submitNewSearch();
         }
     });
+
+    //Page scroll buttons
+    let scrollLeftButton = document.querySelector("#scrollLeft");
+    let scrollRightButton = document.querySelector("#scrollRight");
 
     scrollRightButton.addEventListener("click", async function () {
         if (curr_page < max_page) {
@@ -66,28 +79,22 @@ async function init() {
         }
     });
 
+    //Populate tags
     let tagsSelect = document.getElementById("tagsTitles");
-    // console.log(tagsSelect);
     if (tagsSelect) {
-        addTags(this);
+        addTags();
         let tagButtons = document.getElementsByClassName("tagButton");
         for (let i = 0; i < tagButtons.length; i++) {
-            //e.stopPropagation();
-            tagButtons[i].addEventListener("click", (e) => {
+            tagButtons[i].addEventListener("click", () => {
                 clickedOnATag(tagButtons[i]);
             });
-            /** 
-                        tagButtons[i].addEventListener('mouseover', (e) => {
-                            onHover(tagButtons[i]);
-                        });
-                        tagButtons[i].addEventListener('mouseout', (e) => {
-                            offHover(tagButtons[i]);
-                        });
-                        */
         }
     }
 }
 
+/**
+ * Refreshes displayed recipes with new search results.
+ */
 async function submitNewSearch() {
     /* eslint-disable no-undef */
     let searchResults = await submitInitialSearch(
@@ -102,42 +109,50 @@ async function submitNewSearch() {
         "Page: " + curr_page + " / " + max_page;
 }
 
-function addTags(e) {
+/**
+ * Populates the tags into a grid by category.
+ */
+function addTags() {
     
     //Difficulty
     let tagsList = document.getElementById("tagsListDiff");
     tagsList.style.display = "grid";
-    for (let i of difficultyTags) {
+    for (let name of difficultyTags) {
         let addTagButton = document.createElement("button");
         addTagButton.classList.add("tagButton");
-        addTagButton.innerText = i;
-        addTagButton.id = i;
+        addTagButton.innerText = name;
+        addTagButton.id = name;
         tagsList.appendChild(addTagButton);
     }
 
     //Meal Time
     tagsList = document.getElementById("tagsListMt");
     tagsList.style.display = "grid";
-    for (let i of mealTimeTags) {
+    for (let name of mealTimeTags) {
         let addTagButton = document.createElement("button");
         addTagButton.classList.add("tagButton");
-        addTagButton.innerText = i;
-        addTagButton.id = i;
+        addTagButton.innerText = name;
+        addTagButton.id = name;
         tagsList.appendChild(addTagButton);
     }
 
     //Dietary
     tagsList = document.getElementById("tagsListDiet");
     tagsList.style.display = "grid";
-    for (let i of dietaryTags) {
+    for (let name of dietaryTags) {
         let addTagButton = document.createElement("button");
         addTagButton.classList.add("tagButton");
-        addTagButton.innerText = i;
-        addTagButton.id = i;
+        addTagButton.innerText = name;
+        addTagButton.id = name;
         tagsList.appendChild(addTagButton);
     }
 }
 
+/**
+ * Populates the tags into a grid by category.
+ * 
+ * @param e The tag element
+ */
 function clickedOnATag(e) {
     let index = selectedTags.indexOf(e.innerText);
     if (index > -1) {
@@ -153,26 +168,6 @@ function clickedOnATag(e) {
     }
     submitNewSearch();
 }
-/** 
-function onHover(e) {
-    let index = selectedTags.indexOf(e.innerText);
-    if (index > -1) {
-        e.style.backgroundColor = "#f9c784";
-        e.style.borderColor = "#f9c784";
-    } else {
-        e.style.backgroundColor = "#4e598c";
-        e.style.borderColor = "#4e598c";
-    }
-}
-
-function offHover(e) {
-    let index = selectedTags.indexOf(e.innerText);
-    if (index > -1) {
-        e.style.backgroundColor = "#fcaf58";
-        e.style.borderColor = "#fcaf58";
-    } 
-}
-*/
 
 /**
  * Initial render of recipe cards.
